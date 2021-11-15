022691e1b6bf39fd0008c19dfc1d83cc454a5716
Leandro Camargo <leandro.bahia@cbyk.com.br>
2021-11-15 17:47:39 -0300

    Refatorin screen initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Recruit Devs</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -33,7 +33,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                 <h1>Developer Recruitment</h1>
+                 <h1 style="color: white">Developer Recruitment</h1>
             </div>
         </div>
     </body>
